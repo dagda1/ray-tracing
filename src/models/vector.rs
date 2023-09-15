546026ccdc48ced8fc9c9d1edfaa315a8c6f4b90
Paul Cowan <paul.cowan@cutting.scot>
2023-09-15 18:11:17 +0100

    impl add and test

--- src/models/vector.rs
+++ src/models/vector.rs
@@ -1,9 +1,13 @@
-#[derive(PartialEq, Debug)]
+use std::ops::Add;
+// use std::ops::Sub;
+
+#[derive(PartialEq, Debug, Clone, Copy)]
 pub enum TupleType {
   Vector = 0,
   Point = 1
 }
 
+#[derive(PartialEq, Debug, Clone, Copy)]
 pub struct Tuple {
   pub x: f32,
   pub y: f32,
@@ -19,6 +23,26 @@ impl Tuple {
       TupleType::Vector => Tuple { x: x, y: y, z: z, w: 0.0, is: TupleType::Vector },
       TupleType::Point => Tuple { x: x, y: y, z: z, w: 1.0, is: TupleType::Point }
     }
+  }
+
+  pub fn get_type(self, other: Tuple) -> TupleType {
+    if self.is == TupleType::Point || other.is == TupleType::Point {
+      TupleType::Point
+    } else if self.is == TupleType::Vector && other.is == TupleType::Vector {
+      TupleType::Vector
+    } else {
+      panic!("two Points cannot be added together")
+    }
+  }
+}
+
+impl Add for Tuple {
+  type Output = Tuple;
+
+  fn add(self, other: Tuple) -> Tuple {
+    let tuple_type: TupleType = self.get_type(other);
+    
+    Tuple::new(self.x + other.x, self.y + other.y, self.z + other.z, tuple_type)
   }
 }
 
@@ -56,4 +80,17 @@ use super::*;
       assert_eq!(a.w, 0.0);
       assert_eq!(a.is, TupleType::Vector);
   }
+
+  #[test]
+  fn test_adding_two_tuples() {
+    let a1 = point(3.0,-2.0,5.0);
+    let a2 = vector(-2.0, 3.0, 1.0);
+
+    assert_eq!(a1 + a2, Tuple::new(1.0, 1.0, 6.0, TupleType::Point))
+  }
+
+  // #[test]
+  // fn test_subtracting_two_tuples(){
+
+  // }
 }
