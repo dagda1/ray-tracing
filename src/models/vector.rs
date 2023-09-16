6b1606aa8710a084f0a0cd34f911ff209089b1a7
Paul Cowan <paul.cowan@cutting.scot>
2023-09-16 15:58:26 +0100

    refactor w coordinate

--- src/models/vector.rs
+++ src/models/vector.rs
@@ -19,21 +19,14 @@ pub struct Tuple {
 const ZERO_VECTOR: Tuple = Tuple { x: 0.0, y: 0.0, z: 0.0, w: 0.0, is: TupleType::Vector };
 
 impl Tuple {
-  pub fn new(x: f32, y: f32, z:f32, w: TupleType) -> Self {
-    match w {
-      TupleType::Vector => Tuple { x: x, y: y, z: z, w: 0.0, is: TupleType::Vector },
-      TupleType::Point => Tuple { x: x, y: y, z: z, w: 1.0, is: TupleType::Point }
-    }
-  }
+  pub fn new(x: f32, y: f32, z:f32, w: f32) -> Self {
+    let is = if w == 0.0 {
+                          TupleType::Vector 
+                        } else {
+                          TupleType::Point 
+                        };
 
-  pub fn get_type(self, other: Tuple) -> TupleType {
-    if self.is == TupleType::Point || other.is == TupleType::Point {
-      TupleType::Point
-    } else if self.is == TupleType::Vector && other.is == TupleType::Vector {
-      TupleType::Vector
-    } else {
-      panic!("two Points cannot be added together")
-    }
+    Tuple { x, y, z, w, is }
   }
 
   pub fn negate(self) -> Tuple {
@@ -45,9 +38,7 @@ impl Add for Tuple {
   type Output = Tuple;
 
   fn add(self, other: Tuple) -> Tuple {
-    let tuple_type: TupleType = self.get_type(other);
-    
-    Tuple::new(self.x + other.x, self.y + other.y, self.z + other.z, tuple_type)
+    Tuple::new(self.x + other.x, self.y + other.y, self.z + other.z, self.w + other.w)
   }
 }
 
@@ -55,18 +46,16 @@ impl Sub for Tuple {
   type Output = Tuple;
 
   fn sub(self, other: Tuple) -> Tuple {
-    let tuple_type: TupleType = self.get_type(other);
-    
-    Tuple::new(self.x - other.x, self.y - other.y, self.z - other.z, tuple_type)
+    Tuple::new(self.x - other.x, self.y - other.y, self.z - other.z, (self.w - other.w).abs())
   }
 }
 
 pub fn point(x: f32, y:f32, z:f32) -> Tuple {
-  Tuple::new(x, y, z, TupleType::Point)
+  Tuple::new(x, y, z, 1.0)
 }
 
 pub fn vector(x: f32,  y: f32, z: f32) -> Tuple {
-  Tuple::new(x, y, z, TupleType::Vector)
+  Tuple::new(x, y, z, 0.0)
 }
 
 #[cfg(test)]
@@ -82,6 +71,7 @@ use super::*;
       assert_eq!(a.z, 3.1);
       assert_eq!(a.w, 1.0);
       assert_eq!(a.is, TupleType::Point);
+      assert_ne!(a.is , TupleType::Vector);
   }
 
   #[test]
@@ -93,31 +83,40 @@ use super::*;
       assert_eq!(a.z, 3.1);
       assert_eq!(a.w, 0.0);
       assert_eq!(a.is, TupleType::Vector);
+      assert_ne!(a.is, TupleType::Point);
   }
 
   #[test]
-  fn test_adding_a_vector_to_a_point() {
-    let a1 = point(3.0,-2.0,5.0);
+  fn test_adding_two_tuples() {
+    let a1 = Tuple::new(3.0,-2.0,5.0, 1.0);
     let a2 = vector(-2.0, 3.0, 1.0);
 
-    assert_eq!(a1 + a2, Tuple::new(1.0, 1.0, 6.0, TupleType::Point))
+    assert_eq!(a1 + a2, Tuple::new(1.0, 1.0, 6.0, 1.0))
   }
 
   #[test]
-  fn test_subtracting_a_vector_from_a_point(){
-    let a1 = point(3.0,2.0,1.0);
-    let a2 = vector(5.0, 6.0, 7.0);
+  fn test_subtracting_2_tuples() {
+    let p1 = point(3.0, 2.0, 1.0);
+    let p2 = point(5.0, 6.0, 7.0);
 
-    assert_eq!(a1 - a2, Tuple::new(-2.0, -4.0, -6.0, TupleType::Point))
+    assert_eq!(p1 - p2, vector(-2.0, -4.0, -6.0));
   }
 
-  #[test]
-  fn test_subtracting_2_vectors(){
-    let a1 = vector(3.0,2.0,1.0);
-    let a2 = vector(5.0, 6.0, 7.0);
+  // #[test]
+  // fn test_subtracting_a_vector_from_a_point(){
+  //   let a1 = point(3.0,2.0,1.0);
+  //   let a2 = vector(5.0, 6.0, 7.0);
 
-    assert_eq!(a1 - a2, Tuple::new(-2.0, -4.0, -6.0, TupleType::Vector))
-  }
+  //   assert_eq!(a1 - a2, Tuple::new(-2.0, -4.0, -6.0, 1.0))
+  // }
+
+  // #[test]
+  // fn test_subtracting_2_vectors(){
+  //   let a1 = vector(3.0,2.0,1.0);
+  //   let a2 = vector(5.0, 6.0, 7.0);
+
+  //   assert_eq!(a1 - a2, Tuple::new(-2.0, -4.0, -6.0, 0.0))
+  // }
 
   #[test]
   fn test_subtracting_a_vector_from_the_zero_vector() {
@@ -130,6 +129,6 @@ use super::*;
   fn test_negating_tuple() {
     let a = vector(1.0, -2.0, -3.0);
 
-    assert_eq!(a.negate(), Tuple::new(-1.0, 2.0, 3.0, TupleType::Vector));
+    assert_eq!(a.negate(), Tuple::new(-1.0, 2.0, 3.0, 0.0));
   }
 }
